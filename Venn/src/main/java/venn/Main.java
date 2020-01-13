178914f5d1dd4e348f5cd94d8dc83ff2b04c6bb7
kondepati-revanth-216509838 <revanth@my.yorku.ca>
2020-01-13 13:03:45 -0500

    Test 2 - Rev

--- Venn/src/main/java/venn/Main.java
+++ Venn/src/main/java/venn/Main.java
@@ -4,38 +4,6 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		System.out.println("print");
-		System.out.println("new print");
-
-		System.out.println("Update 1");		
-
-		/*test try.. */
-		
-		/*
-		 * Test for pushV
-		 */
-
-		System.out.println(3 + 5 * 8 + 9);
-		int j = 2302; int eecsNum = 2311; String prof = "Tzerpos";
-		
-	/* test for push */
-	/* Test 2 - R */
-	/* Snoop Dogg */
-
-		// test for push V2
-		// test again
-
-		
-		for(int i =0;i<5;i++) {
-			System.out.println("print");
-		}
-
-	}
-
-	int xx = 1;//ygygvb /*jfsldjflsdjf*/ 
-	int y = 2;
-	// Nonsense dfsjdfljfljs dsjdaslkdas
-	String w = "string";
- /*EECS 2311ß*/
-	boolean b = true;
+System.out.println("Google");
+}
 }
