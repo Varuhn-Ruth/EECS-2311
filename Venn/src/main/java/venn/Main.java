feb93cc46f79cb0ef24270c1d82193264ba45ce6
kondepati-revanth-216509838 <revanth@my.yorku.ca>
2020-01-24 14:50:05 -0500

    Erased old code in Main class

--- Venn/src/main/java/venn/Main.java
+++ Venn/src/main/java/venn/Main.java
@@ -4,42 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		System.out.println("print");
-		System.out.println("new print");//jjj
+	//Removed bs
 		
-
-		/*test try.. *///gfgf
-		
-		/*
-		 * Test for pushV
-		 */
-		
-		// test for push V2
-		// test again
-
-
-		boolean x = true;
-		if(x) {
-			System.out.println("print");
-		}
-		
-		int y = 0;
-
-		int z = 4;
-		int w = 1;
-		
-		String s = "string";
-		
-		int xx = 0;
-		for(int i =0;i<5;i++) {
-			System.out.println("print");
-		}
-
-	}
-
-	int xx = 1;
-	int y = 2;
 	
-	String w = "string";
-	//Test Again
 }
