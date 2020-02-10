5e5e403de0df8dec572986900fb92bf0fb4ebb84
kondepati-revanth-216509838 <revanth@my.yorku.ca>
2020-02-09 19:39:42 -0500

    Typed CMD + SHIFT + F

--- Venn/src/main/java/venn/RevCode.java
+++ Venn/src/main/java/venn/RevCode.java
@@ -5,28 +5,28 @@ import javax.swing.JLabel;
 
 public class RevCode {
 
-	static JLabel Title[] = new JLabel[1];
+	static JLabel title[] = new JLabel[1];
 
 	// Initiated Class
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
 	}
 
-	//This code runs when user clicks the finish button when they create a label
+	// This code runs when user clicks the finish button when they create a label
 	static void addLabel(JLabel newLabel, JLabel Array[]) {
-		JLabel result[] = new JLabel[Title.length + 1];
+		JLabel result[] = new JLabel[title.length + 1];
 		for (int i = 0; i < result.length; i++) {
-			if (i == Title.length) {
+			if (i == title.length) {
 				result[i] = newLabel;
 			} else {
 				result[i] = Array[i];
 			}
 		}
 	}
-	
+
 	static void removeLabel() {
-		
+
 	}
-	
 
 }
