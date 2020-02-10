eeb0ad60fe8252fa9710bc7b80aa418d035c4caa
kondepati-revanth-216509838 <revanth@my.yorku.ca>
2020-02-09 19:06:01 -0500

    Wrote code to add a label variable. Made an array to contain labels that act as titles.

--- Venn/src/main/java/venn/RevCode.java
+++ Venn/src/main/java/venn/RevCode.java
@@ -1,12 +1,32 @@
 package venn;
 
+import javax.*;
+import javax.swing.JLabel;
+
 public class RevCode {
-	//Initiated Class
+
+	static JLabel Title[] = new JLabel[1];
+
+	// Initiated Class
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
 	}
 
+	//This code runs when user clicks the finish button when they create a label
+	static void addLabel(JLabel newLabel, JLabel Array[]) {
+		JLabel result[] = new JLabel[Title.length + 1];
+		for (int i = 0; i < result.length; i++) {
+			if (i == Title.length) {
+				result[i] = newLabel;
+			} else {
+				result[i] = Array[i];
+			}
+		}
+	}
+	
+	static void removeLabel() {
+		
+	}
+	
+
 }
-
-
