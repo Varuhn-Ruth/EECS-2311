bf00aa61234bb04dcd14d9621af1281e1d5a5bef
Ruthirakuhan-Varuhn-215634140 <vruth11@my.yorku.ca>
2020-03-07 18:36:51 -0500

    changed to add font edit styles

--- Venn/vM/Implementation/application/Main.java
+++ Venn/vM/Implementation/application/Main.java
@@ -30,6 +30,7 @@ import javafx.scene.effect.BlendMode;
 import javafx.scene.effect.ColorInput;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.DragEvent;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Background;
@@ -51,10 +52,6 @@ public class Main extends Application {
 	Scene scn1;
 	Circle cir1 =  new Circle (400,340,200, Color.rgb(255, 255, 255, 0.2));
 	Circle cir2 = new Circle (590,340,200, Color.rgb(255,255,255,0.2));
-	Circle cirs1 =  new Circle (410,300,150, Color.rgb(255, 255, 255, 0.5));
-	Circle cirs2 =  new Circle (560,300,150, Color.rgb(255, 255, 255, 0.5));
-	Circle cirb1 =  new Circle (360,340,250, Color.rgb(255, 255, 255, 0.5));
-	Circle cirb2 =  new Circle (600,340,250, Color.rgb(255, 255, 255, 0.5));
 	Scene scn;
 	Scene scn3;
 	Scene scn2;
@@ -85,15 +82,13 @@ public class Main extends Application {
 				//add circles and buttons coordinates x,y,r 
 
 				cir1.setStroke(Color.BLACK);
-				
+				cir1.toFront();
 				cir2.setStroke(Color.BLACK);
+				cir2.toBack();
 				
 				Blend blend = new Blend();
 				blend.setMode(BlendMode.MULTIPLY);
-				
-				cirb1.setStroke(Color.BLACK);			
-				cirb2.setStroke(Color.BLACK);
-				
+					
 				//initialize new colorpicker utility
 				choose1.setPrefSize(100, 30);
 				choose1.setLayoutX(735);
@@ -106,11 +101,13 @@ public class Main extends Application {
 			    for(int i = 0; i < 50; i++) {
 					TextField insert = new TextField();
 					insert.setPrefSize(170, 50);
-					insert.setLayoutX(100);
-					insert.setLayoutY(100);
-					insert.setBackground(null);
+					insert.setLayoutX(10);
+					insert.setLayoutY(100);	
 					insert.setText("o ");
 					insert.setOnMouseDragged(event -> drag(event));
+					insert.setOnMouseReleased(event -> released(event));
+					
+					
 					root.getChildren().add(i, insert);
 					
 				}
@@ -124,185 +121,12 @@ public class Main extends Application {
 			    
 			    //
 				
-//				ChoiceBox<String> size1 = new ChoiceBox<>();
-//				size1.setPrefSize(75, 20);
-//				size1.setValue("Medium");
-//				size1.getItems().add("Small");
-//				size1.getItems().add("Medium");
-//				size1.getItems().add("Large");
-//				size1.setLayoutX(10);
-//				size1.setLayoutY(20);
 				
 				Button sz1 = new Button("Set Size");
 				sz1.setPrefSize(80, 20);
 				sz1.setLayoutX(180);
 				sz1.setLayoutY(20);
-				
-				TextField lm1 = new TextField();
-				lm1.setPrefSize(150, 40);
-				lm1.setLayoutX(300);
-				lm1.setLayoutY(120);
-				lm1.setBackground(null); //this is it
-				lm1.setText("o First point");
-				
-				TextField lm2 = new TextField();
-				lm2.setPrefSize(170, 40);
-				lm2.setLayoutX(250);
-				lm2.setLayoutY(160);
-				lm2.setBackground(null);
-				lm2.setText("o Second point");
-				
-				TextField lm3 = new TextField();
-				lm3.setPrefSize(170, 40);
-				lm3.setLayoutX(220);
-				lm3.setLayoutY(200);
-				lm3.setBackground(null); 
-				lm3.setText("o Third point");
-				
-				TextField lm4 = new TextField();
-				lm4.setPrefSize(170, 40);
-				lm4.setLayoutX(205);
-				lm4.setLayoutY(240);
-				lm4.setBackground(null); 
-				lm4.setText("o Fourth point");
-				
-				TextField lm5 = new TextField();
-				lm5.setPrefSize(170, 40);
-				lm5.setLayoutX(205);
-				lm5.setLayoutY(280);
-				lm5.setBackground(null); 
-				lm5.setText("o Fifth point");
-				
-				TextField lm6 = new TextField();
-				lm6.setPrefSize(170, 40);
-				lm6.setLayoutX(205);
-				lm6.setLayoutY(320);
-				lm6.setBackground(null); 
-				lm6.setText("o Sixth point");
-				
-				TextField lm7 = new TextField();
-				lm7.setPrefSize(170, 40);
-				lm7.setLayoutX(220);
-				lm7.setLayoutY(360);
-				lm7.setBackground(null); 
-				lm7.setText("o Seventh point");
-				
-				TextField lm8 = new TextField();
-				lm8.setPrefSize(170, 40);
-				lm8.setLayoutX(250);
-				lm8.setLayoutY(400);
-				lm8.setBackground(null); 
-				lm8.setText("o Eighth point");
-				
-				TextField lm9 = new TextField();
-				lm9.setPrefSize(170, 40);
-				lm9.setLayoutX(300);
-				lm9.setLayoutY(440);
-				lm9.setBackground(null); 
-				lm9.setText("o Ninth point");
-				
-				//middle
-				
-				TextField mm1 = new TextField();
-				mm1.setPrefSize(170, 40);
-				mm1.setLayoutX(410);
-				mm1.setLayoutY(200);
-				mm1.setBackground(null); 
-				mm1.setText("• First similarity");
-				
-				TextField mm2 = new TextField();
-				mm2.setPrefSize(170, 40);
-				mm2.setLayoutX(395);
-				mm2.setLayoutY(240);
-				mm2.setBackground(null); 
-				mm2.setText("• Second similarity");
-				
-				TextField mm3 = new TextField();
-				mm3.setPrefSize(170, 40);
-				mm3.setLayoutX(395);
-				mm3.setLayoutY(280);
-				mm3.setBackground(null); 
-				mm3.setText("• Third similarity");
-				
-				TextField mm4 = new TextField();
-				mm4.setPrefSize(170, 40);
-				mm4.setLayoutX(395);
-				mm4.setLayoutY(320);
-				mm4.setBackground(null); 
-				mm4.setText("• Fourth similarity");
-				
-				TextField mm5 = new TextField();
-				mm5.setPrefSize(170, 40);
-				mm5.setLayoutX(410);
-				mm5.setLayoutY(360);
-				mm5.setBackground(null); 
-				mm5.setText("• Fifth similarity");
-				
-				// left section medium
-				
-				TextField rm1 = new TextField();
-				rm1.setPrefSize(150, 40);
-				rm1.setLayoutX(530);
-				rm1.setLayoutY(120);
-				rm1.setBackground(null); //this is it
-				rm1.setText("o First point");
-				
-				TextField rm2 = new TextField();
-				rm2.setPrefSize(170, 40);
-				rm2.setLayoutX(560);
-				rm2.setLayoutY(160);
-				rm2.setBackground(null);
-				rm2.setText("o Second point");
-				
-				TextField rm3 = new TextField();
-				rm3.setPrefSize(170, 40);
-				rm3.setLayoutX(585);
-				rm3.setLayoutY(200);
-				rm3.setBackground(null); 
-				rm3.setText("o Third point");
-				
-				TextField rm4 = new TextField();
-				rm4.setPrefSize(170, 40);
-				rm4.setLayoutX(595);
-				rm4.setLayoutY(240);
-				rm4.setBackground(null); 
-				rm4.setText("o Fourth point");
-				
-				TextField rm5 = new TextField();
-				rm5.setPrefSize(170, 40);
-				rm5.setLayoutX(600);
-				rm5.setLayoutY(280);
-				rm5.setBackground(null); 
-				rm5.setText("o Fifth point");
-				
-				TextField rm6 = new TextField();
-				rm6.setPrefSize(170, 40);
-				rm6.setLayoutX(595);
-				rm6.setLayoutY(320);
-				rm6.setBackground(null); 
-				rm6.setText("o Sixth point");
-				
-				TextField rm7 = new TextField();
-				rm7.setPrefSize(170, 40);
-				rm7.setLayoutX(585);
-				rm7.setLayoutY(360);
-				rm7.setBackground(null); 
-				rm7.setText("o Seventh point");
-			
-				TextField rm8 = new TextField();
-				rm8.setPrefSize(170, 40);
-				rm8.setLayoutX(560);
-				rm8.setLayoutY(400);
-				rm8.setBackground(null); 
-				rm8.setText("o Eighth point");
-				
-				TextField rm9 = new TextField();
-				rm9.setPrefSize(170, 40);
-				rm9.setLayoutX(530);
-				rm9.setLayoutY(440);
-				rm9.setBackground(null); 
-				rm9.setText("o Ninth point");
-				
+
 //				new line
 				
 				TextField ls1 = new TextField();
@@ -603,28 +427,33 @@ public class Main extends Application {
 				
 				// draw small size points
 				
-				cirs1.setStroke(Color.BLACK);
-				
-				cirs2.setStroke(Color.BLACK);
-				
+//				cirs1.setStroke(Color.BLACK);
+//				
+//				cirs2.setStroke(Color.BLACK);
+//				
 				Button rs = new Button("Set Topic One Colour");
 				rs.setPrefSize(150, 20);
 				rs.setLayoutX(840);
 				rs.setLayoutY(20);
 				
 				ChoiceBox<String> choicebox3 = new ChoiceBox<>();
-				choicebox3.setPrefSize(75, 20);
-				choicebox3.setValue("Red");
-				choicebox3.getItems().add("Red");
-				choicebox3.getItems().add("Orange");
-				choicebox3.getItems().add("Yellow");
-				choicebox3.getItems().add("Green");
-				choicebox3.getItems().add("Blue");
-				choicebox3.getItems().add("Indigo");
-				choicebox3.getItems().add("Violet");
-				choicebox3.getItems().add("White");
-				choicebox3.setLayoutX(760);
-				choicebox3.setLayoutY(20);
+				choicebox3.setPrefSize(100, 20);
+				choicebox3.setValue("Times New Roman");
+				choicebox3.getItems().add("Verdana");
+				choicebox3.getItems().add("Times New Roman");
+				choicebox3.getItems().add("Arial");
+				choicebox3.getItems().add("Arial Black");
+				choicebox3.getItems().add("Calibri");
+//				choicebox3.getItems().add("Indigo");
+//				choicebox3.getItems().add("Violet");
+//				choicebox3.getItems().add("White");
+				choicebox3.setLayoutX(10);
+				choicebox3.setLayoutY(400);
+				
+				Button changefont = new Button("Set font");
+				changefont.setPrefSize(100, 20);
+				changefont.setLayoutX(120);
+				changefont.setLayoutY(400);
 				
 				Button ls = new Button("Set Topic Two Colour");
 				ls.setPrefSize(150, 20);
@@ -835,17 +664,11 @@ public class Main extends Application {
 				root.getChildren().add(cir2);
 
 			    root.getChildren().add(sz1);
-//			    root.getChildren().add(size1);
-			    root.getChildren().addAll(lm1, lm2, lm3, lm4, lm5, lm6, lm7, lm8, lm9);
-			    root.getChildren().addAll(mm1, mm2, mm3, mm4, mm5);
-			    root.getChildren().addAll(rm1, rm2, rm3, rm4, rm5, rm6, rm7, rm8, rm9);
-			    root.getChildren().addAll(Label1, Label2, Label3, Label4);
+			    root.getChildren().addAll(Label1, Label2, Label3, Label4, choicebox3, changefont);
 			    root.setEffect(blend);
 			    root.getChildren().addAll(button2, choose1, choose2, cc1, cc2, slider);
 			    root.getChildren().addAll(capture1, display);
-			    
-			    
-				
+			    			
 				scn = new Scene(root,1000,600);
 				scn.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 				primaryStage.setTitle("Venn Diagram");
@@ -867,8 +690,6 @@ public class Main extends Application {
 				primaryStage.setScene(scn3);
 				primaryStage.show();
 				
-				root2.getChildren().add(cirb1);
-				root2.getChildren().add(cirb2);
 				root2.getChildren().add(rb);
 				root2.getChildren().add(lb);
 				root2.getChildren().add(choicebox5);
@@ -882,11 +703,9 @@ public class Main extends Application {
 				root2.getChildren().addAll(ll1, ll2, ll3, ll4, ll5, ll6, ll7, ll8, ll9, ll10, ll11);
 				root2.getChildren().addAll(capture2, display2);
 				
-				root3.getChildren().add(cirs1);
-				root3.getChildren().add(cirs2);
 				root3.getChildren().add(rs);
 				root3.getChildren().add(ls);
-				root3.getChildren().add(choicebox3);
+//				root3.getChildren().add(choicebox3);
 				root3.getChildren().add(choicebox4);
 				root3.getChildren().add(size2);
 				root3.getChildren().add(sz2);
@@ -978,14 +797,11 @@ public class Main extends Application {
 
 					cc1.setOnAction(e -> getChoicecc1(choose1));
 					cc2.setOnAction(e -> getChoicecc2(choose2));
+					changefont.setOnAction(e -> getFont(choicebox3));
 				
-					rs.setOnAction(e -> getChoice3(choicebox3));
-					ls.setOnAction(e -> getChoice4(choicebox4));
 					sz1.setOnAction(e -> changeSize(slider));					
 					sz2.setOnAction(e -> changeSizeS(size2));
 					
-					rb.setOnAction(e -> getChoice5(choicebox5));
-					lb.setOnAction(e -> getChoice6(choicebox6));
 					sz3.setOnAction(e -> changeSizeB(size3));
 					
 					layout1.getChildren().addAll(label1, t1, textfield1, t2, textfield2, t3, textfield3, button1);
@@ -996,10 +812,16 @@ public class Main extends Application {
 				}
 
 	
+	private void released(MouseEvent event) {
+		TextField t = (TextField)event.getSource();
+		t.setBackground(null);
+	}
+
+
 	private void drag(MouseEvent event) {
 		Node n = (Node)event.getSource();
 		n.setTranslateX(n.getTranslateX() + event.getX());
-		n.setTranslateY(n.getTranslateY() + event.getY());
+		n.setTranslateY(n.getTranslateY() + event.getY());		
 		
 	}
 
@@ -1007,7 +829,7 @@ public class Main extends Application {
 	private void getChoicecc1(ColorPicker choose1) {
 		Color color = choose1.getValue();
 		if(color != null) {
-			cir1.setFill(color.deriveColor(1, 1, 1, 0.5));
+			cir1.setFill(color.deriveColor(1, 1, 1, 0.3));
 		}
 		
 	}
@@ -1015,68 +837,19 @@ public class Main extends Application {
 	private void getChoicecc2(ColorPicker choose2) {
 		Color color = choose2.getValue();
 		if(color != null) {
-			cir2.setFill(color.deriveColor(1, 1, 1, 0.5));
+			cir2.setFill(color.deriveColor(1, 1, 1, 0.3));
 		}
 		
 	}
+	
+	private void getFont(ChoiceBox<String> choicebox3) {
+		String font = choicebox3.getValue();
+		
+		if(font == "Verdana") {
+//			insert.setFont()
+		}
+	}
 				
-	
-	
-	private void getChoice3(ChoiceBox<String> choicebox3) {
-		String color = choicebox3.getValue();
-		if(color == "Red") {
-			cirs1.setFill(Color.rgb(255,0,0, 0.5));
-		}
-		if(color == "Orange") {
-			cirs1.setFill(Color.rgb(255,165,0, 0.5));
-		}
-		if(color == "Yellow") {
-			cirs1.setFill(Color.rgb(255,255,0, 0.5));
-		}
-		if(color == "Green") {
-			cirs1.setFill(Color.rgb(0,255,0, 0.5));
-		}
-		if(color == "Blue") {
-			cirs1.setFill(Color.rgb(0,0,255, 0.5));
-		}
-		if(color == "Indigo") {
-			cirs1.setFill(Color.rgb(75,0,130, 0.5));
-		}
-		if(color == "Violet") {
-			cirs1.setFill(Color.rgb(128,0,128, 0.5));
-		}
-		if(color == "White") {
-			cirs1.setFill(Color.rgb(255,255,255, 0.5));
-		}
-	}	
-	
-	private void getChoice4(ChoiceBox<String> choicebox4) {
-		String color = choicebox4.getValue();
-		if(color == "Red") {
-			cirs2.setFill(Color.rgb(255,0,0, 0.5));
-		}
-		if(color == "Orange") {
-			cirs2.setFill(Color.rgb(255,165,0, 0.5));
-		}
-		if(color == "Yellow") {
-			cirs2.setFill(Color.rgb(255,255,0, 0.5));
-		}
-		if(color == "Green") {
-			cirs2.setFill(Color.rgb(0,255,0, 0.5));
-		}
-		if(color == "Blue") {
-			cirs2.setFill(Color.rgb(0,0,255, 0.5));
-		}
-		if(color == "Indigo") {
-			cirs2.setFill(Color.rgb(75,0,130, 0.5));
-		}
-		if(color == "Violet") {
-			cirs2.setFill(Color.rgb(128,0,128, 0.5));
-		}
-		if(color == "White") {
-			cirs2.setFill(Color.rgb(255,255,255, 0.5));
-		}
-	}			
 	
 	private void changeSize(Slider slider) {
 		double size = slider.getValue();
@@ -1098,62 +871,6 @@ public class Main extends Application {
 			window.setScene(scn2);
 		}
 	}
-	
-	private void getChoice5(ChoiceBox<String> choicebox5) {
-		String color = choicebox5.getValue();
-		if(color == "Red") {
-			cirb1.setFill(Color.rgb(255,0,0, 0.5));
-		}
-		if(color == "Orange") {
-			cirb1.setFill(Color.rgb(255,165,0, 0.5));
-		}
-		if(color == "Yellow") {
-			cirb1.setFill(Color.rgb(255,255,0, 0.5));
-		}
-		if(color == "Green") {
-			cirb1.setFill(Color.rgb(0,255,0, 0.5));
-		}
-		if(color == "Blue") {
-			cirb1.setFill(Color.rgb(0,0,255, 0.5));
-		}
-		if(color == "Indigo") {
-			cirb1.setFill(Color.rgb(75,0,130, 0.5));
-		}
-		if(color == "Violet") {
-			cirb1.setFill(Color.rgb(128,0,128, 0.5));
-		}
-		if(color == "White") {
-			cirb1.setFill(Color.rgb(255,255,255, 0.5));
-		}
-	}		
-	
-	private void getChoice6(ChoiceBox<String> choicebox6) {
-		String color = choicebox6.getValue();
-		if(color == "Red") {
-			cirb2.setFill(Color.rgb(255,0,0, 0.5));
-		}
-		if(color == "Orange") {
-			cirb2.setFill(Color.rgb(255,165,0, 0.5));
-		}
-		if(color == "Yellow") {
-			cirb2.setFill(Color.rgb(255,255,0, 0.5));
-		}
-		if(color == "Green") {
-			cirb2.setFill(Color.rgb(0,255,0, 0.5));
-		}
-		if(color == "Blue") {
-			cirb2.setFill(Color.rgb(0,0,255, 0.5));
-		}
-		if(color == "Indigo") {
-			cirb2.setFill(Color.rgb(75,0,130, 0.5));
-		}
-		if(color == "Violet") {
-			cirb2.setFill(Color.rgb(128,0,128, 0.5));
-		}
-		if(color == "White") {
-			cirb2.setFill(Color.rgb(255,255,255, 0.5));
-		}
-	}		
 	
 	private void changeSizeB(ChoiceBox<String> size3) {
 		String size = size3.getValue();
